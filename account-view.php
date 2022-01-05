251cd85d321b96d3504eed4bcae7804684d9833d
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-05 17:18:55 +0100

    Added edit account functionality

--- account-view.php
+++ account-view.php
@@ -1,7 +1,7 @@
 <?php
 $pageTitle = "My Account | LingoBuddy";
 include "header.php";
-$user = $_SESSION["user"]
+$user = $_SESSION["user"];
 ?>
 <main class="page contact-us-page">
 
@@ -9,6 +9,29 @@ $user = $_SESSION["user"]
         <div class="container">
             <div class="block-heading">
                 <h2 class="text-info">My Account</h2>
+                <p><?php
+
+                    if (isset($_POST['submit'])) {
+                        $email = $_POST['email'];
+                        $first_name = $_POST['first_name'];
+                        $last_name = $_POST['last_name'];
+                        $country = $_POST['country'];
+                        $password = $_POST['password'];
+                        $phone_number = $_POST['phone_number'];
+
+                        $sql = 'UPDATE user SET email ="' . $_POST['email'] . '" WHERE id = 101';
+
+                        // $sql = "UPDATE user SET email=:email, location=:location WHERE id =:101";
+                        $servername = "localhost";
+                        $dbname = "lingobuddy";
+                        $conn = new PDO("mysql:host=$servername;dbname=$dbname", 'root', 'root');
+                        $statement = $conn->prepare($sql);
+
+                        if ($statement->execute($data)) {
+                            echo '<p style="color:green; font-weight: bold;"> Account updated successfully!</p>';
+                        }
+                        unset($_POST['submit']);
+                    }; ?></p>
                 <p>View your account details and recent orders.</p>
             </div>
             <div class="account-container">
@@ -19,7 +42,7 @@ $user = $_SESSION["user"]
                                 <p class="mb-0">Name</p>
                             </div>
                             <div class="col-sm-9">
-                                <p class="text-muted mb-0"><?php echo user_name($user) ?></p>
+                                <p class="text-muted mb-0"><?php echo user_first_name($user) . " " . user_last_name($user) ?></p>
                             </div>
                         </div>
                         <hr>
@@ -49,7 +72,9 @@ $user = $_SESSION["user"]
                                 <p class="text-muted mb-0"><?php echo user_country($user) ?></p>
                             </div>
                         </div>
+
                     </div>
+                    <p class="editacc"><a href="/editaccount.php">Edit account</a></p>
                 </div>
                 <table class="table table-striped table-hover">
                     <thead>
--- connect.php
+++ connect.php
@@ -9,6 +9,11 @@ $dbdatabase = 'lingobuddy';
 
 $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbdatabase);
 
+function connection()
+{
+    return new mysqli('localhost', 'root', 'root', 'lingbuddy');
+}
+
 // Check connection
 // if ($mysqli === false) {
 // die("ERROR: Could not connect. " . $mysqli->connect_error);
@@ -34,14 +39,12 @@ $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbdatabase);
 function find_order($user)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = "SELECT * FROM `user_order` WHERE `user` =" . $user . ";";
-
     $result = $mysqli->query($sql);
-
     $mysqli->close();
-    while ($row = $result->fetch_assoc()) {
-        echo '<tr>
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>
                         <th scope="row">' . $row["id"] . '</th>
                         <td>' . $row["word_count"] . '</td>
                         <td>' . $row["price"] . '</td>
@@ -49,7 +52,8 @@ function find_order($user)
                         <td>' . $row["file_url"] . '</td>
                         <td>Edit</td>
                     </tr>';
-    }
+        }
+    } else echo '<tr><td>No orders found</td></tr>';
 }
 
 function log_out()
@@ -61,14 +65,11 @@ function log_out()
 function languages_spoken($translator)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = "SELECT name FROM language JOIN translator ON language.id = translator.language 
         OR language.id = translator.language_2 OR language.id = translator.language_3 WHERE translator.id = " . $translator . ";";
-
     $result = $mysqli->query($sql);
     $languages = array();
     $output = "";
-
     $mysqli->close();
     while ($row = $result->fetch_assoc()) {
         $languages[] = $row["name"];
@@ -84,36 +85,50 @@ function languages_spoken($translator)
     return $output;
 }
 
-function user_name($user)
+function user_first_name($user)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
-    $sql = "SELECT `first_name`, `last_name` FROM `user` WHERE `id`=" . $user . ";";
-
+    $sql = "SELECT `first_name` FROM `user` WHERE `id`=" . $user . ";";
     $result = $mysqli->query($sql);
     $row = $result->fetch_assoc();
     $mysqli->close();
-    return $row["first_name"] . " " . $row["last_name"];
+    return $row["first_name"];
+}
+
+function user_last_name($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+    $sql = "SELECT `last_name` FROM `user` WHERE `id`=" . $user . ";";
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["last_name"];
 }
 
 function user_email($user)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = "SELECT `email` FROM `user` WHERE `id`=" . $user . ";";
-
     $result = $mysqli->query($sql);
     $row = $result->fetch_assoc();
     $mysqli->close();
     return $row["email"];
 }
 
+function user_password($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+    $sql = "SELECT `password` FROM `user` WHERE `id`=" . $user . ";";
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["password"];
+}
+
 function user_phone($user)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = "SELECT `phone_number` FROM `user` WHERE `id`=" . $user . ";";
-
     $result = $mysqli->query($sql);
     $row = $result->fetch_assoc();
     $mysqli->close();
@@ -123,9 +138,7 @@ function user_phone($user)
 function user_country($user)
 {
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = "SELECT `country` FROM `user` WHERE `id`=" . $user . ";";
-
     $result = $mysqli->query($sql);
     $row = $result->fetch_assoc();
     $mysqli->close();
@@ -134,22 +147,16 @@ function user_country($user)
 
 function price_calc($word): int
 {
-    if ($word * 0.18 < 25) {
-        return 25;
-    }
+    if ($word * 0.18 < 25) return 25;
     return 1000 * 0.18;
 }
 
 function log_in($email, $password)
 {
-
     // $_POST["email"];
 
-
     $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
-
     $sql = 'SELECT id FROM `user` WHERE email = "' . $email . '" AND password = "' . $password . '";';
-
     $result = $mysqli->query($sql);
     $row = $result->fetch_assoc();
     $mysqli->close();
