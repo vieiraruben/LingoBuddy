f88438d8922e10521c1ccd1d641a12c393304604
thibault <thibault.blanchard@edu.itescia.fr>
2022-01-05 11:01:12 +0100

    Update pricing.php
    Adaptation de la page pricing a notre activité

--- pricing.php
+++ pricing.php
@@ -6,8 +6,8 @@ include "header.php";
     <section class="clean-block clean-pricing dark">
         <div class="container">
             <div class="block-heading">
-                <h2 class="text-info">Pricing Table</h2>
-                <p>Check how much your translation will cost by entering your document's word count.</p>
+                <h2 class="text-info">Service prices</h2>
+                <p>Here are some examples of prices</p>
             </div>
             <div class="row justify-content-center">
                 <div class="col-md-5 col-lg-4">
@@ -15,15 +15,14 @@ include "header.php";
                         <div class="heading">
                             <h3>BASIC</h3>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>For casual use</p>
                         <div class="features">
-                            <h4><span class="feature">Full Support:&nbsp;</span><span>No</span></h4>
-                            <h4><span class="feature">Duration:&nbsp;</span><span>30 Days</span></h4>
-                            <h4><span class="feature">Storage:&nbsp;</span><span>10GB</span></h4>
+                            <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>2</span></h4>
+                            <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>no</span></h4>
                         </div>
                         <div class="price">
                             <h4>$25</h4>
-                        </div><button class="btn btn-outline-primary d-block w-100" type="button">BUY NOW</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-5 col-lg-4">
@@ -32,15 +31,14 @@ include "header.php";
                         <div class="heading">
                             <h3>PRO</h3>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>For normal use</p>
                         <div class="features">
-                            <h4><span class="feature">Full Support:&nbsp;</span><span>Yes</span></h4>
-                            <h4><span class="feature">Duration:&nbsp;</span><span>60 Days</span></h4>
-                            <h4><span class="feature">Storage:&nbsp;</span><span>50GB</span></h4>
+                            <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>5</span></h4>
+                            <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>no</span></h4>
                         </div>
                         <div class="price">
                             <h4>$50</h4>
-                        </div><button class="btn btn-primary d-block w-100" type="button">BUY NOW</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-5 col-lg-4">
@@ -48,15 +46,14 @@ include "header.php";
                         <div class="heading">
                             <h3>PREMIUM</h3>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>For intensive use</p>
                         <div class="features">
-                            <h4><span class="feature">Full Support:&nbsp;</span><span>Yes</span></h4>
-                            <h4><span class="feature">Duration:&nbsp;</span><span>120 Days</span></h4>
-                            <h4><span class="feature">Storage:&nbsp;</span><span>150GB</span></h4>
+                        <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>20</span></h4>
+                            <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>yes</span></h4>
                         </div>
                         <div class="price">
                             <h4>$150</h4>
-                        </div><button class="btn btn-outline-primary d-block w-100" type="button">BUY NOW</button>
+                        </div>
                     </div>
                 </div>
             </div>
