d97e866eb1a0051677f52e0135b2dfca21ba6696
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-05 12:48:14 +0100

    Added account view and user detail functions

---
+++ account-view.php
@@ -0,0 +1,72 @@
+<?php
+$pageTitle = "My Account | LingoBuddy";
+include "header.php";
+$user = $_SESSION["user"]
+?>
+<main class="page contact-us-page">
+
+    <section class="clean-block clean-form dark">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-info">My Account</h2>
+                <p>View your account details and recent orders.</p>
+            </div>
+            <div class="account-container">
+                <div class="card mb-4 contact-details">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Name</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0"><?php echo user_name($user) ?></p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Email</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0"><?php echo user_email($user) ?></p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Phone</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0"><?php echo user_phone($user) ?></p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <p class="mb-0">Country</p>
+                            </div>
+                            <div class="col-sm-9">
+                                <p class="text-muted mb-0"><?php echo user_country($user) ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Word Count</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Created On</th>
+                            <th scope="col">File</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php find_order($user) ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include "footer.php"; ?>
--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION["user"] = 101;
 
 $dbhost = 'localhost';
 $dbuser = 'root';
@@ -30,6 +31,26 @@ $mysqli = new mysqli($dbhost, $dbuser, $dbpassword, $dbdatabase);
 //     echo "0 results";
 // }
 // $conn->close();
+function find_order($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+
+    $sql = "SELECT * FROM `user_order` WHERE `user` =" . $user . ";";
+
+    $result = $mysqli->query($sql);
+
+    $mysqli->close();
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>
+                        <th scope="row">' . $row["id"] . '</th>
+                        <td>' . $row["word_count"] . '</td>
+                        <td>' . $row["price"] . '</td>
+                        <td>' . $row["created_on"] . '</td>
+                        <td>' . $row["file_url"] . '</td>
+                        <td>Edit</td>
+                    </tr>';
+    }
+}
 
 function log_out()
 {
@@ -61,4 +82,60 @@ function languages_spoken($translator)
         }
     }
     return $output;
+}
+
+function user_name($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+
+    $sql = "SELECT `first_name`, `last_name` FROM `user` WHERE `id`=" . $user . ";";
+
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["first_name"] . " " . $row["last_name"];
+}
+
+function user_email($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+
+    $sql = "SELECT `email` FROM `user` WHERE `id`=" . $user . ";";
+
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["email"];
+}
+
+function user_phone($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+
+    $sql = "SELECT `phone_number` FROM `user` WHERE `id`=" . $user . ";";
+
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["phone_number"];
+}
+
+function user_country($user)
+{
+    $mysqli = new mysqli('localhost', 'root', 'root', 'lingobuddy');
+
+    $sql = "SELECT `country` FROM `user` WHERE `id`=" . $user . ";";
+
+    $result = $mysqli->query($sql);
+    $row = $result->fetch_assoc();
+    $mysqli->close();
+    return $row["country"];
+}
+
+function price_calc($word): int
+{
+    if ($word * 0.18 < 25) {
+        return 25;
+    }
+    return 1000 * 0.18;
 }
