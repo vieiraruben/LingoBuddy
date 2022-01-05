ab6ac47683ef4ebce6f86f85f7c50cdb7bd80ee4
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-05 10:05:10 +0100

    Created log out function

--- header.php
+++ header.php
@@ -1,33 +1,35 @@
-   <!DOCTYPE html>
-   <html>
+<?php include "connect.php"; ?>
 
-   <head>
-       <meta charset="utf-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-       <title><?= $pageTitle ?></title>
-       <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap">
-       <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-       <link rel="stylesheet" href="assets/bootstrap/css/vanilla-zoom.min.css">
-       <link rel="stylesheet" href="assets/bootstrap/css/style.css">
+<!DOCTYPE html>
+<html>
 
-   </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/vanilla-zoom.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/style.css">
 
-   <body>
-       <header>
-           <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
-               <div class="container"><a class="navbar-brand logo" href="#"><img class="img-logo" src="assets/img/language-translator.png" width="50" height="50">LingoBuddy</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                   <div class="collapse navbar-collapse" id="navcol-1">
-                       <ul class="navbar-nav ms-auto">
-                           <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-                           <li class="nav-item"><a class="nav-link" href="features.php">Features</a></li>
-                           <li class="nav-item"><a class="nav-link" href="pricing.php">Pricing</a></li>
-                           <li class="nav-item"><a class="nav-link" href="translators.php">Translators</a></li>
-                           <li class="nav-item"><a class="nav-link" href="contact-us.php">Sign In</a></li>
-                       </ul>
-                   </div>
-               </div>
-           </nav>
-       </header>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+            <div class="container"><a class="navbar-brand logo" href="#"><img class="img-logo" src="assets/img/language-translator.png" width="50" height="50">LingoBuddy</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="features.php">Features</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pricing.php">Pricing</a></li>
+                        <li class="nav-item"><a class="nav-link" href="translators.php">Translators</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact-us.php">Sign In</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
