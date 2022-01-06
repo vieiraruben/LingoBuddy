cd3448145cf8bfd435ff6148debbc7c8a884a943
HD-4Code <86689864+HD-4Code@users.noreply.github.com>
2022-01-06 09:53:08 +0100

    account update
    update of account and database con.

--- account.php
+++ account.php
@@ -60,7 +60,7 @@ function random_num($length){
 
 <div class="wrapper">
     <div class="text-center mt-4 name"> Login </div>
-    <form class="p-3 mt-3" method="POST" action="account-view">
+    <form class="p-3 mt-3" method="POST" action="account-view.php">
         <div class="form-field d-flex align-items-center"> <span class="far fa-user"></span> <input type="text" name="user_name" id="userName" placeholder="Username" required> </div>
         <div class="form-field d-flex align-items-center"> <span class="fas fa-key"></span> <input type="password" name="password" id="pwd" placeholder="Password" required> </div> <button class="btn mt-3">Login</button>
     </form>
