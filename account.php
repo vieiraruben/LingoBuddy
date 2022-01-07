dfd1978eda681fe03a77f6dd6f61f741db2a268c
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-07 10:35:18 +0100

    Fixed edit account link

--- account.php
+++ account.php
@@ -4,38 +4,19 @@ include "header.php";
 ?>
 
 <?php
+function check_login($dbConnection)
+{
 
-// if($_SERVER['REQUEST_METHOD']== "POST"){
-//     // if something was posted
-//     $user_name = $_POST['user_name'];
-//     $password = $_POST['password'];
-
-//     if(!empty($user_name)&& !empty($password) && !is_numeric($user_name)){
-
-//         //$user_id = random_num(20);
-//         $query = "insert into users (user_id, user_name, password) values ('$user_id','$user_name','$password')";
-
-//         mysqli_query($dbConnection, $query);
-//         header("Location : account-view.php");
-//         die;
-//     }else{
-//         echo "Please enter a valid information!";
-//     }
-// }
-
-function check_login($dbConnection){
-
-    if(isset($_SESSION['user_id'])){
+    if (isset($_SESSION['user_id'])) {
 
         $id = $_SESSION['user_id'];
         $query = "select * from users where user_id = '$id' limit 1";
 
         $result = mysqli_query($dbConnection, $query);
-        if($result && mysqli_num_rows($result) > 0){
+        if ($result && mysqli_num_rows($result) > 0) {
 
             $user_data = mysqli_fetch_assoc($result);
             return $user_data;
-
         }
     }
     // redirect to login
@@ -43,15 +24,16 @@ function check_login($dbConnection){
     die;
 }
 
-function random_num($length){
+function random_num($length)
+{
     $text = "";
-    if($length < 5){
+    if ($length < 5) {
         $length = 5;
     }
-    $len = rand(4,$length);
-    for ($i=0; $i < $len; $i++) { 
+    $len = rand(4, $length);
+    for ($i = 0; $i < $len; $i++) {
         #---
-        $text .= rand(0,9);
+        $text .= rand(0, 9);
     }
     return $text;
 }
