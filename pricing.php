5bfb361911374e985b74f935ab41ff6050008719
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-07 12:03:23 +0100

    Changed pricing

--- pricing.php
+++ pricing.php
@@ -7,7 +7,7 @@ include "header.php";
         <div class="container">
             <div class="block-heading">
                 <h2 class="text-info">Our offers</h2>
-                <p>Choose the offer adapted to your needs !</p>
+                <p>Choose the offer adapted to your needs!</p>
             </div>
             <div class="row justify-content-center">
                 <div class="col-md-5 col-lg-4">
@@ -17,11 +17,11 @@ include "header.php";
                         </div>
                         <p>For casual use</p>
                         <div class="features">
-                            <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>2</span></h4>
+                            <h4><span class="feature">Number of documents uploaded:&nbsp;</span><span>2</span></h4>
                             <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>no</span></h4>
                         </div>
                         <div class="price">
-                            <h4>$25</h4>
+                            <h4>€25</h4>
                         </div>
                     </div>
                 </div>
@@ -33,11 +33,11 @@ include "header.php";
                         </div>
                         <p>For normal use</p>
                         <div class="features">
-                            <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>5</span></h4>
+                            <h4><span class="feature">Number of documents uploaded:&nbsp;</span><span>5</span></h4>
                             <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>no</span></h4>
                         </div>
                         <div class="price">
-                            <h4>$50</h4>
+                            <h4>€50</h4>
                         </div>
                     </div>
                 </div>
@@ -48,11 +48,11 @@ include "header.php";
                         </div>
                         <p>For intensive use</p>
                         <div class="features">
-                        <h4><span class="feature">Number of document uploaded:&nbsp;</span><span>20</span></h4>
+                            <h4><span class="feature">Number of documents uploaded:&nbsp;</span><span>20</span></h4>
                             <h4><span class="feature">Intervention of a translator:&nbsp;</span><span>yes</span></h4>
                         </div>
                         <div class="price">
-                            <h4>$150</h4>
+                            <h4>€150</h4>
                         </div>
                     </div>
                 </div>
