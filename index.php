5c48195125777cca32f04e8a95d52215d4fd8ed8
AmalGuedda <guedda94320@gmail.com>
2022-01-07 12:39:34 +0100

    Validate code
    Validate code on HTML

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include "header.php";
 <main class="page landing-page">
     <section class="clean-block clean-hero" style="color:rgba(255, 255, 255);">
         <div class="d-flex justify-content-around">
-            <img class="text" src="assets/img/language-translator.png" width="400" height="400">
+            <img class="text" src="assets/img/language-translator.png" alt="" width="400" height="400">
             <div class="text flex-column d-flex justify-content-around">
                 <h1>Welcome to LingoBuddy</h1>
                 <h2>Get your professional translation from a real interpreter now.</h2>
@@ -22,9 +22,9 @@ include "header.php";
             <div class="block-heading">
                 <h2 class="text-info">Entrust your content to our experienced interpreters!</h2>
              </div>   
-             <div class="row justify-content-center">
+            <div class="row justify-content-center">
                 <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/solution.jpg">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/solution.jpg" alt="">
                         <div class="card-body info">
                             <h4 class="card-title">Turn-key solution</h4>
                             <p class="card-text">Do you need a translation of a document ? Just import it and you will have your document translated and sent within hours or days! </p>
@@ -32,7 +32,7 @@ include "header.php";
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/best.jpg">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/best.jpg" alt="" >
                         <div class="card-body info">
                             <h4 class="card-title">Excellency</h4>
                             <p class="card-text">Our interpreters have a minimum of 5 years experience. Moreover, they have all studied or lived abroad </p>
@@ -40,7 +40,7 @@ include "header.php";
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/data.jpg">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/data.jpg" alt="">
                         <div class="card-body info">
                             <h4 class="card-title">Protection of your data</h4>
                             <p class="card-text">We are an European company, which applies the regulations of personal data protection. Your content remains your full and exclusive property.</p>
@@ -48,7 +48,7 @@ include "header.php";
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>   
     </section>
     <section class="clean-block slider dark">
         <div class="container">
