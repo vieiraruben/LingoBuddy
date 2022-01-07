c7bda6563bbba2b931903dfda760b9d5ea78405c
AmalGuedda <guedda94320@gmail.com>
2022-01-07 09:24:05 +0100

    Summary
    Add Front-end

--- header.php
+++ header.php
@@ -22,8 +22,8 @@
             <div class="container"><a class="navbar-brand logo" href="#"><img class="img-logo" src="assets/img/language-translator.png" width="50" height="50">LingoBuddy</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navcol-1">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="pricing.php">Pricing</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="pricing.php">Our offers</a></li>
                         <li class="nav-item"><a class="nav-link" href="translators.php">Translators</a></li>
                         <?php if (logged_in()) {
                             echo '<li class="nav-item"><a class="nav-link" href="account-view.php">Account</a></li>'
--- index.php
+++ index.php
@@ -13,64 +13,55 @@ include "header.php";
             <div class="text flex-column d-flex justify-content-around">
                 <h1>Welcome to LingoBuddy</h1>
                 <h2>Get your professional translation from a real interpreter now.</h2>
-                <button class="btn btn-outline-light btn-lg" type="button">Learn More</button>
+                <a href="pricing.php" class="btn btn-outline-dark" role="button" aria-pressed="true">Learn More</a>
             </div>
         </div>
     </section>
-    <section class="clean-block clean-info dark">
+    section class="clean-block clean-info dark">
         <div class="container">
             <div class="block-heading">
-                <h2 class="text-info">Info</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-            </div>
-            <div class="row align-items-center">
-                <div class="col-md-6"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg"></div>
-                <div class="col-md-6">
-                    <h3>Lorem impsum dolor sit amet</h3>
-                    <div class="getting-started-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    </div><button class="btn btn-outline-primary btn-lg" type="button">Join Now</button>
+                <h2 class="text-info">Entrust your content to our experienced interpreters!</h2>
+             </div>   
+             <div class="row justify-content-center">
+                <div class="col-sm-6 col-lg-4">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/solution.jpg">
+                        <div class="card-body info">
+                            <h4 class="card-title">Turn-key solution</h4>
+                            <p class="card-text">Do you need a translation of a document ? Just import it and you will have your document translated and sent within hours or days! </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/best.jpg">
+                        <div class="card-body info">
+                            <h4 class="card-title">Excellency</h4>
+                            <p class="card-text">Our interpreters have a minimum of 5 years experience. Moreover, they have all studied or lived abroad </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/data.jpg">
+                        <div class="card-body info">
+                            <h4 class="card-title">Protection of your data</h4>
+                            <p class="card-text">We are an European company, which applies the regulations of personal data protection. Your content remains your full and exclusive property.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="clean-block features">
-        <div class="container">
-            <div class="block-heading">
-                <h2 class="text-info">Features</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+            <div class="d-flex justify-content-center">
+                <a href="pricing.php" class="btn btn-outline-dark" role="button" aria-pressed="true">Discover our offers! </a>      
             </div>
-            <div class="row justify-content-center">
-                <div class="col-md-5 feature-box"><i class="icon-star icon"></i>
-                    <h4>Bootstrap 4</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <div class="col-md-5 feature-box"><i class="icon-pencil icon"></i>
-                    <h4>Customizable</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <div class="col-md-5 feature-box"><i class="icon-screen-smartphone icon"></i>
-                    <h4>Responsive</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-                <div class="col-md-5 feature-box"><i class="icon-refresh icon"></i>
-                    <h4>All Browser Compatibility</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-                </div>
-            </div>
-        </div>
+        </div>    
     </section>
     <section class="clean-block slider dark">
         <div class="container">
             <div class="block-heading">
-                <h2 class="text-info">Slider</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                <h2 class="text-info">Our partners who recommend us !</h2>
             </div>
             <div class="carousel slide" data-bs-ride="carousel" id="carousel-1">
                 <div class="carousel-inner">
-                    <div class="carousel-item active"><img class="w-100 d-block" src="assets/img/scenery/image1.jpg" alt="Slide Image"></div>
-                    <div class="carousel-item"><img class="w-100 d-block" src="assets/img/scenery/image4.jpg" alt="Slide Image"></div>
-                    <div class="carousel-item"><img class="w-100 d-block" src="assets/img/scenery/image6.jpg" alt="Slide Image"></div>
+                    <div class="carousel-item active"><img class="w-100 d-block" src="assets/img/peinture.jpg " alt="Slide Image"></div>
+                    <div class="carousel-item"><img class="w-100 d-block" src="assets/img/paris.jpg" alt="Slide Image"></div>
                 </div>
                 <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
                 <ol class="carousel-indicators">
@@ -78,43 +69,6 @@ include "header.php";
                     <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
                     <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
                 </ol>
-            </div>
-        </div>
-    </section>
-    <section class="clean-block about-us">
-        <div class="container">
-            <div class="block-heading">
-                <h2 class="text-info">About Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar1.jpg">
-                        <div class="card-body info">
-                            <h4 class="card-title">John Smith</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar2.jpg">
-                        <div class="card-body info">
-                            <h4 class="card-title">Robert Downturn</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card text-center clean-card"><img class="card-img-top w-100 d-block" src="assets/img/avatars/avatar3.jpg">
-                        <div class="card-body info">
-                            <h4 class="card-title">Ally Sanders</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                            <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
