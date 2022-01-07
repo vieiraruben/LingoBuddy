8fabbc0cd008e3317248cc1b435b1df9795a54ae
vieiraruben <86010862+vieiraruben@users.noreply.github.com>
2022-01-07 11:30:10 +0100

    Added profile pic

--- order-recap.php
+++ order-recap.php
@@ -1,5 +1,8 @@
 <?php
 $pageTitle = "Confirm Order | LingoBuddy";
+
+
+
 session_start();
 $target_dir = getcwd() . DIRECTORY_SEPARATOR;
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -22,7 +25,13 @@ if ($_FILES["fileToUpload"]["name"] != "") {
     $_SESSION["upload_msg"] = "";
 };
 include "header.php";
+
+
+
 ?>
+
+
+
 <main class="page contact-us-page">
     <section class="clean-block clean-form dark">
         <div class="container">
